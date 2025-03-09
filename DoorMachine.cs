9c2d01d9627af591262e572d424be3857247f915
baalaal <147219396+baalaal@users.noreply.github.com>
2025-03-10 03:32:37 +0700

    git add . menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class DoorMachine
+{
+    // Enum untuk menyimpan state (status) pintu
+    private enum State { Terkunci, Terbuka }
+
+    // Variabel untuk menyimpan state saat ini
+    private State currentState = State.Terkunci;
+
+    // Method untuk mengunci pintu
+    public void Lock()
+    {
+        currentState = State.Terkunci;
+        Console.WriteLine("Pintu terkunci");
+    }
+
+    // Method untuk membuka kunci pintu
+    public void Unlock()
+    {
+        currentState = State.Terbuka;
+        Console.WriteLine("Pintu tidak terkunci");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,5 +7,12 @@ class Program
         // Memanggil method GetKodePos dari class KodePos
         string kodePos = KodePos.GetKodePos("Batununggal");
         Console.WriteLine($"Kode Pos Batununggal: {kodePos}");
+
+        // Membuat objek dari class DoorMachine
+        DoorMachine door = new DoorMachine();
+
+        // Mensimulasikan perubahan state
+        door.Unlock(); // Pintu tidak terkunci
+        door.Lock();   // Pintu terkunci
     }
 }
